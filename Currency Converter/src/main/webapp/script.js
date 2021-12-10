b631fd2c0ab4f69776bb56ea94cae7b094e27d3a
Anthony Lapham <lapham.2@wright.edu>
2021-12-10 00:21:31 -0500

    -add script tag and hide/show functions

--- Currency Converter/src/main/webapp/script.js
+++ Currency Converter/src/main/webapp/script.js
@@ -1,4 +1,10 @@
 $(document).ready(function(){
 	
 	$("#countryConversion").hide();
+	$(".country").click(function(event){
+		event.stopPropagation();
+        event.stopImmediatePropagation();
+        $('#currentCountry').hide();
+        $('#countryConversion').show();
+	});
 });
